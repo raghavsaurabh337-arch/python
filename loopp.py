e28145db63ec437ea77c72bfb84dd2c8118d3584
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-03 23:18:37 +0530

    pattren 5*5

--- loopp.py
+++ loopp.py
@@ -5,5 +5,11 @@ class pattren  :
      def five(self):
           for i in range(1,6):
                print("*")
+     def five_by_five(self):
+          for i in range(1,6):
+               for j in range(1,6):
+                    print("*", end=" ")
+               print()
 obj=pattren()
 obj.five()
+obj.five_by_five()
