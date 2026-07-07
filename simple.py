42ff68274479e1c97aea29798be99b8836687d02
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-07 22:59:01 +0530

    function user

--- simple.py
+++ simple.py
@@ -1,7 +1,9 @@
 class Student:
      name="saurabh Raghav"
+     def show(self):
+         print(self.name)
 
 s1=Student() 
 s2=Student()  
-print(s1.name) 
-print(s2.name)    
+# print(s1.name) 
+s1.show()   
